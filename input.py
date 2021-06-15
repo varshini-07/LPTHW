94fbae390e91679aeebbaf0942d9d951211fcea4
varshini-07 <78426186+varshini-07@users.noreply.github.com>
2021-06-15 11:07:57 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,9 @@
+print("how old are you", end='')
+age =input()
+print("how tall are you?",end='')
+height =input()
+print("how much do you weigh?",end='')
+weight =input()
+
+print("so you're {age} old,{height} tall and {weight} heavy." )
+
