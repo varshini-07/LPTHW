4847f06eb21d3fb7f808541ca91e86c2a4da3f7d
varshini-07 <78426186+varshini-07@users.noreply.github.com>
2021-07-08 19:43:37 +0530

    Add files via upload

---
+++ comment.py
@@ -0,0 +1,9 @@
+# a comment ,this is so you can read your program later.
+# anything after the # is ignored by python 
+
+print("I could have code like this.") # and the comment after is ignored
+
+# you can also use a comment to "disable" or comment out a piece of code:
+# print"this won't run."
+
+print("this will run")
