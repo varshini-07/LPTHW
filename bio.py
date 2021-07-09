2ace103837c9f00849ff05bf912d6840f46cfa29
varshini-07 <78426186+varshini-07@users.noreply.github.com>
2021-07-09 19:17:41 +0530

    Add files via upload

---
+++ bio.py
@@ -0,0 +1,17 @@
+my_name='zed A shaw'
+my_age=35 
+my_height=74
+my_weight=180
+my_eyes='blue'
+my_teeth='white'
+my_hair='brown'
+
+print("let talk about %s" % my_name)
+print("he %d inches tall" % my_height)
+print("he %d pounds heavy" % my_weight)
+print("actually thats not too heavy")
+print("he got %s eyes and %s hair" % (my_eyes,my_hair))
+print("his teeth are usually %s depending on the coffee" % (my_teeth))
+
+
+print("ifi add %d,%d,and %d i get %d" % (my_age,my_height,my_weight,my_age+my_height+my_weight)) 
