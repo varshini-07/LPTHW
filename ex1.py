4a158be3b088a853a8d3c43e27e6160ab94b11e7
varshini-07 <78426186+varshini-07@users.noreply.github.com>
2021-07-15 19:54:54 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,28 @@
+x="there are %d types of people" % 10
+binary = "binary"
+do_not="dont"
+y="those who know %s and those who %s,"% (binary,do_not)
+
+print (x)
+print (y)
+
+print("i said: %r." % x)
+print("i also said:'%s'." % y)
+
+hilarious = False
+joke_evaluation="isnt that joke so funny?! %r"
+
+print(joke_evaluation % hilarious)
+
+w=("this is the left side of...")
+e=("a strong with a right side.")
+
+print(w+e)
+
+# you can debug like this also wat u do ..do from first i m sry
+f = 100
+print ("w=", w)
+print ("f is =", f)
+
+
+
